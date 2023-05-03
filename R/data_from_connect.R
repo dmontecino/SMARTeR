a7c32bf8dd3c34777c613f7d639aae00ac4dae09
dmontecino <dmontecino@ucdavis.edu>
2023-05-03 11:35:21 -0400

    correcting username left over

--- R/data_from_connect.R
+++ R/data_from_connect.R
@@ -53,7 +53,7 @@ data_from_connect<-function(server_url,
     form.unfilled = session_connect %>% html_node("form") %>% html_form()
     
     form.filled = form.unfilled %>%
-      html_form_set("j_username" = username,
+      html_form_set("j_username" = user,
                     "j_password" = password)
     
     #login
