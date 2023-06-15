81e7b9cded207090163d25290cb6d76a9f99fc23
dmontecino <dmontecino@ucdavis.edu>
2023-06-15 11:06:51 -0400

    changes how the elements of each query per conservation area are found and moved to a data frame (api.queries.4) and improving labeling and documentation

--- R/queries_available_per_conservation_area.R
+++ R/queries_available_per_conservation_area.R
@@ -56,17 +56,45 @@ queries_available_per_conservation_area<-function(
   # a flatten list of the data for the queries for each conservation area
   api.queries.3<-lapply(api.queries.2, flattenlist)
   
-  #queries available per conservation area 
+  # finding the information of queries per conservation area
+  
+  #> find elements with the key words "items" and "uuid" or "subFolders" and
+  #> "items", and "uuids"
+  api.queries.per.CA<-
+    map(api.queries.3, \(x)
+        x[grep("items.*uuid|subFolders.*items.*uuid", 
+               names(x), 
+               perl = TRUE)]) %>% 
+    map(function(y) unlist(y, use.names = F))
+  
+  #> find elements with the key words "items" and "name" or "subFolders" and
+  #> "items", and "name" 
+  name.queries.per.CA<-
+    map(api.queries.3, 
+        \(x) x[grep("items.*name|subFolders.*items.*name", names(x))]) %>% 
+    map(function(y) unlist(y, use.names = F))
+  
+  #> find elements with the key words "items" and "type" but not "typeKey" or
+  #>"subFolders" and items", and "type" but not "typeKey"
+  type.queries.per.CA<-
+    map(api.queries.3, 
+        \(x) x[grep("items.*type(?!Key)|subFolders.*items.*type(?!Key)", 
+                    names(x), 
+                    perl = TRUE)]) %>% 
+    map(function(y) unlist(y, use.names = F))
+  
+  #queries available per conservation area in a dataset format
   api.queries.4<-
-    
-    lapply(api.queries.3, function(x)  
-      data.frame(
-        query_api=x$items.uuid,
-        query_name=x$items.name,
-        query_type=x$items.type))
+
+  pmap(list(api.queries.per.CA, 
+     name.queries.per.CA,
+     type.queries.per.CA),
+     function(x,y,z) {data.frame(api_query=x, 
+                              name_query=y,
+                              type_query=z)})
       
-      
-  if(all(sapply(sapply(api.queries.4, "[[", "query_name"), length)==0)){
+  #checking if any query is available for any CA    
+  if(all(sapply(sapply(api.queries.4, "[[", "name_query"), length)==0)){
     stop("there are no queries available in connect")}
 
   # assign the names of the conservation areas
