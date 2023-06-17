aaac2a429d395c94b01628a142a9b84141bc188b
dmontecino <dmontecino@ucdavis.edu>
2023-06-17 06:45:04 -0400

    changes how the output is returned

--- R/create_species_attribute.R
+++ R/create_species_attribute.R
@@ -21,8 +21,8 @@ create_data_model_with_translated_species_attribute<-
   function(path, # path to the excel document with the species "your_path/wildlife_species_list_template.xlsx"
            number_of_sheets, # number of sheets in the excel document. Each sheet represents a taxonomy class.
            translated_language=NULL, # if you just want to create a species attribute with the english names given in the .xlsx file, then leave it as NULL
-           species_in_translated_language=NULL, # spell "species" in the language of interest (NULL if english)
-           save_to){ #path where the data model with the species attibute should be constructed
+           species_in_translated_language=NULL){# spell "species" in the language of interest (NULL if english)
+           #path where the data model with the species attibute should be constructed
 
     
 sheets<-1:number_of_sheets
@@ -163,23 +163,25 @@ xml_text <- readLines(temp_file)
 xml_text[1]<-'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
 
 #save the xml with the data model that contains only a species attribute
-writeLines(xml_text, con = paste0(save_to, "/", "species_smarter_attribute.xml"))
+writeLines(xml_text, con = "species_smarter_attribute.xml")
 
 # Remove the temporary file
 file.remove(temp_file)
+
+return(xml_text)
 
   }
 
 # run function
 
-path<-"/wildlife_species_list_template.xlsx"
+path<-"wildlife_species_list_template.xlsx"
 number_of_sheets<-4
 translated_language="lo"
 species_in_translated_language="ຊະນິດ"
-save_to<-""
+# save_to<-""
 
-# species_translated<-create_data_model_with_translated_species_attribute(
-#   path=path, 
-#   number_of_sheets=number_of_sheets,
-#   translated_language=translated_language,
-#   species_in_translated_language=species_in_translated_language)
+create_data_model_with_translated_species_attribute(
+  path=path,
+  number_of_sheets=number_of_sheets,
+  translated_language=translated_language,
+  species_in_translated_language=species_in_translated_language)
