71c79dae0d301055b9d60a6c725393ad9c49bde9
dmontecino <dmontecino@ucdavis.edu>
2023-06-25 13:02:53 -0400

    major changes to get the information about the folders in each CA where the queries are located

--- R/queries_available_per_conservation_area.R
+++ R/queries_available_per_conservation_area.R
@@ -1,24 +1,11 @@
 
 library(rvest)
+library(plyr)
 library(tidyverse)
 library(jsonlite)
+library(purrr)
 
-# --------------------------------------------- #
-# function to flatten a list to a single level  #
-# --------------------------------------------- #
-
-flattenlist <- function(x){  
-    morelists <- sapply(x, function(xprime) class(xprime)[1]=="list")
-    out <- c(x[!morelists], unlist(x[morelists], recursive=FALSE))
-    if(sum(morelists)){ 
-      Recall(out)
-    }else{
-      return(out)
-    }
-  }
-
-
-
+#
 # ------------------------------------------------------------------------#
 # function to check the conservation areas in connect you have access to  #
 # and the corresponding queries within that conservation area             #
@@ -55,67 +42,73 @@ queries_available_per_conservation_area<-function(
   #names of the conservation areas in connect and with queries available
   names.conservation.areas<-sapply(api.queries.2, function(x) x$name)
   
-  # a flatten list of the data for the queries for each conservation area
-  api.queries.3<-lapply(api.queries.2, flattenlist)
+  #queries by conservation area by folder,including folders wo any queries
+  api.queries.3<-
+    map(seq_along(api.queries.2), function(x){
+      list(dplyr::tibble(query=api.queries.2[x]) %>% 
+             tidyr::unnest_wider(query) %>% 
+             dplyr::rename(folder=name) %>% 
+             dplyr::select(-caUuid) %>% 
+             tidyr::unnest(items) %>% 
+             tidyr::unnest_wider(items) %>% 
+             dplyr::select(-c( type, id, isShared, folderUuid, iconName, isCcaa, conservationArea)) %>% 
+             dplyr::rename(query_name=name) %>% 
+             dplyr::select(folder, caUuid, query_name, typeKey, uuid, subFolders))})
   
-  # finding the information of queries per conservation area
   
-  #> find elements with the key words "items" and "uuid" or "subFolders" and
-  #> "items", and "uuids"
-  api.queries.per.CA<-
-    map(api.queries.3, \(x)
-        x[grep("items.*uuid|subFolders.*items.*uuid", 
-               names(x), 
-               perl = TRUE)]) %>% 
-    map(function(y) unlist(y, use.names = F))
   
-  #> find elements with the key words "items" and "name" or "subFolders" and
-  #> "items", and "name" 
-  name.queries.per.CA<-
-    map(api.queries.3, 
-        \(x) x[grep("items.*name|subFolders.*items.*name", names(x))]) %>% 
-    map(function(y) unlist(y, use.names = F))
-  
-  #> find elements with the key words "items" and "type" but not "typeKey" or
-  #>"subFolders" and items", and "type" but not "typeKey"
-  type.queries.per.CA<-
-    map(api.queries.3, 
-        \(x) x[grep("items.*typeKey|subFolders.*items.*typeKey", 
-                    names(x), 
-                    perl = TRUE)]) %>% 
-    map(function(y) unlist(y, use.names = F))
-  
-  #queries available per conservation area in a dataset format
-  api.queries.4<-
+  for(i in seq_along(api.queries.3)){
+    
+    counter <- 1
+    
+    while(!all(is.na(api.queries.3[[i]][[counter]]$subFolders) | 
+               is.null(api.queries.3[[i]][[counter]]$subFolders))){ 
+      #> if not all subfolders are empty
+      #> if at least one CA has a  subfolder
 
-  pmap(list(api.queries.per.CA, 
-     name.queries.per.CA,
-     type.queries.per.CA),
-     function(x,y,z) {data.frame(query_api=x, 
-                              query_name=y,
-                              query_type=z)})
+      api.queries.3[[i]][[counter+1]]<-
+        #queries per folder
+        api.queries.3[[i]][[counter]] %>% 
+        dplyr::select(subFolders) %>% 
+        tidyr::unnest(subFolders) %>% 
+        tidyr::unnest_wider(subFolders) %>% 
+        dplyr::relocate(subFolders, .after = last_col()) %>% 
+        dplyr::rename(folder=name) %>% 
+        dplyr::select(-caUuid) %>% 
+        tidyr::unnest(items, keep_empty = T) %>% 
+        tidyr::unnest_wider(items) %>% 
+        dplyr::select(-c( type, id, isShared, folderUuid, iconName, isCcaa, conservationArea)) %>% 
+        dplyr::rename(query_name=name) %>% 
+        dplyr::distinct() %>% 
+        dplyr::filter_all(any_vars(!is.na(.)))
       
+      api.queries.3[[i]][[counter+1]]$folder<-
+        paste0(rep(api.queries.3[[i]][[counter]]$folder,  
+                   map_vec(api.queries.3[[i]][[counter]]$subFolders, length)),
+               "/",
+               api.queries.3[[i]][[counter+1]]$folder)
+      
+      
+      counter <- counter + 1
+    }
+    
+    
+  }
+  
+  api.queries.4<-map(api.queries.3, \(x) x %>% 
+                 reduce(full_join) %>%  
+                 dplyr::select(-caUuid, -subFolders))
+  
+  
   #checking if any query is available for any CA    
   if(all(sapply(sapply(api.queries.4, "[[", "query_name"), length)==0)){
     stop("there are no queries available in connect")}
 
-  #add information about executable queries
+  #add information about spatial queries
   api.queries.5<-map(api.queries.4,  ~ .x %>%
-        
-    mutate(executable=query_type%in%
-        c('patrolobservation', 'patrolquery', 'patrolwaypoint',
-           'patrolsummary', 'patrolgrid', 'observationobservation',
-           'observationwaypoint', 'observationsummary', 'observationgrid',
-           'entityobservation', 'entitywaypoint', 'entitysummary',
-           'entitygrid', 'surveyobservation', 'surveywaypoint',
-           'surveysummary', 'surveygrid', 'surveymission',
-           'surveymissiontrack', 'assetobservation', 'assetwaypoint',
-           'assetsummary', 'assetdeploymentsummary', 'i2_obs_query',
-           'i2_entity_summ_query', 'i2_entity_record_query', 
-           'i2_record_query', 'i2_record_summ_query')) %>% 
-      
+  
     # learn if the query hs spatial information
-    mutate(spatial_query=query_type%in%
+    mutate(spatial_query=typeKey%in%
           c("entityobservation", "entitywaypoint","intelligencerecord",  "surveymission",
             "surveymissiontrack", "observationobservation", "observationwaypoint", 
             "patrolobservation", "patrolquery", "patrolwaypoint", 
@@ -150,12 +143,12 @@ queries_available_per_conservation_area<-function(
   
   date_filter_types_available_per_query <- 
     date_filter_types_available_per_query %>%
-    rownames_to_column(var = "query_type")
+    rownames_to_column(var = "typeKey")
   
   # Perform the left join based on row names
   api.queries.6 <- 
     map(api.queries.5, \(x)
-        left_join(x, date_filter_types_available_per_query, by = "query_type"))
+        left_join(x, date_filter_types_available_per_query, by = "typeKey"))
   
   
    # assign the names of the conservation areas
